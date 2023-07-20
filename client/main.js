baef050eb7bc2a7280ec16b3aabf98f5a31faabe
cfgop23 <cfgop23@gmail.com>
2023-07-20 16:48:10 +0900

    04.jujeob 브랜치 생성

--- client/main.js
+++ client/main.js
@@ -1,6 +1,1 @@
-/* eslint-disable */
-/* eslint-enable */
-// /* eslint no-unused-vars: 0 */ 주석에 직접 rule을 적용할 수도 있다.
-
-console.log('hello js!');
-const a = 10;
+import { getNode } from './lib/index.js';
