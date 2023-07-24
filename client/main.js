11361a09a74f9162d563e106daad1bd0043d8df7
cfgop23 <cfgop23@gmail.com>
2023-07-24 12:00:46 +0900

    05.randomDice 브랜치 생성

--- client/main.js
+++ client/main.js
@@ -1,6 +1,3 @@
-/* eslint-disable */
-/* eslint-enable */
-// /* eslint no-unused-vars: 0 */ 주석에 직접 rule을 적용할 수도 있다.
+import { diceAnimation } from './lib/index.js';
 
-console.log('hello js!');
-const a = 10;
+diceAnimation();
